dc346dd01f153fc7184cc8524b89353f9ee9f1a3
Personal-IliyanDimov <ilianmd@abv.bg>
2021-01-10 23:39:34 +0200

    Implement ES store logic (Part1)

--- src/main/java/org/imd/cqrs/sample1/es/model/postgres/AggregateEntity.java
+++ src/main/java/org/imd/cqrs/sample1/es/model/postgres/AggregateEntity.java
@@ -3,14 +3,12 @@ package org.imd.cqrs.sample1.es.model.postgres;
 import lombok.Getter;
 import lombok.Setter;
 import org.imd.cqrs.sample1.es.model.shared.AggregateType;
-import org.springframework.data.cassandra.core.mapping.Table;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
 @Setter
-@Table
 public class AggregateEntity {
 
     private AggregateType type;
